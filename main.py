a66c89c62f932a0d323984f105fca140cecb19d3
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-03 12:53:33 +0600

    Pandas init

---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+data = {'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+        'Age': [25, 30, 35, 28],
+        'City': ['New York', 'San Francisco', 'Los Angeles', 'Chicago']}
+
+df = pd.DataFrame(data)
+
+
+# Accesing full data farem
+print(df)
+print("=============")
+
+print(df["Name"])
+print("=============")
+
+# Accessing Rows:
+print(df.iloc[1])
+print("=============")
+
+# Slicing:
+print(df[['Name', 'Age']])
+print(df[1:3])
+print("=============")
+
+# Finding Unique Elements:
+print(df['Age'].unique())
+print("=============")
+
+# Conditional Filtering:
+high_above_25 = df[df['Age'] > 25 ]
+print(high_above_25)
+print("=============")
