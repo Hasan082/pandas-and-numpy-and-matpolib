c8e8e9d2e260a54f6459f6e79934261d5cf8b797
Md Hasanuzzaman <dr.has82@gmail.com>
2025-01-04 21:52:39 +0600

    Image donwlaod ans show

--- rquest_in_python.py
+++ rquest_in_python.py
@@ -14,4 +14,16 @@ header = response.headers
 print(header['date'])
 print(header['content-type'])
 print(response.encoding)
-print(response.text[0:100])
+# print(response.text[0:100])
+# Image
+url2 = ('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN'
+        '-SkillsNetwork/IDSNlogo.png')
+
+res = requests.get(url2)
+
+path = os.path.join(os.getcwd(), 'image.png')
+
+with open(path, 'wb') as f:
+    f.write(res.content)
+
+print(Image.open(path))
